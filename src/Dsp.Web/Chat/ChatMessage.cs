490a7731a5e55a7f5369dcbaf3c98d586708f80b
joncloud <jdberube@gmail.com>
2015-04-04 15:28:08 -0700

    Added logic for parsing auto translate text.

--- src/Dsp.Web/Chat/ChatMessage.cs
+++ src/Dsp.Web/Chat/ChatMessage.cs
@@ -11,10 +11,52 @@ namespace Dsp.Web.Chat
         public string Message { get; set; }
         public byte[] MessageContents
         {
-            set { Message = Encoding.UTF8.GetString(value); }
+            set { Message = Deserialize(value); }
         }
         public DateTime Occurrence { get; set; }
         public string Recipient { get; set; }
         public string Speaker { get; set; }
+
+        private static string Deserialize(byte[] contents)
+        {
+            StringBuilder text = new StringBuilder();
+            bool translate = false;
+            int start = 0;
+            for (int i = 0; i < contents.Length; i++)
+            {
+                byte b = contents[i];
+
+                // 0xFD surrounds the auto translate information.
+                if (b == 0xFD)
+                {
+                    if (translate)
+                    {
+                        translate = false;
+
+                        // For now just print the values indicating that this is an auto translate text.
+                        text.Append("(")
+                             .Append(BitConverter.ToString(contents, start + 1, i - start - 1))
+                             .Append(")");
+                        start = i;
+                    }
+                    else
+                    {
+                        translate = true;
+
+                        // Print all of the text up until the auto translate text.
+                        text.Append(Encoding.UTF8.GetString(contents, start, i - start));
+                        start = i;
+                    }
+                }
+            }
+
+            // If there was no auto translate text, then just convert the text.
+            if (text.Length == 0 && contents.Length > 0)
+            {
+                text.Append(Encoding.UTF8.GetString(contents));
+            }
+
+            return text.ToString();
+        }
     }
 }
